a6886f5c462945fa929ff7288305b6a714466006
Drew Graham <dmg9626@gmail.com>
2016-10-23 09:59:27 -0400

    hhh

---
+++ Trump - An Interactive Experience/Assets/Scripts/BadHombreMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BadHombreMove : MonoBehaviour 
+{
+	public bool CanMove;
+	public float MoveSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		CanMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (CanMove) 
+		{
+			transform.position = new Vector2(transform.position.x - MoveSpeed * Time.deltaTime, transform.position.y);
+		}
+	}
+}
