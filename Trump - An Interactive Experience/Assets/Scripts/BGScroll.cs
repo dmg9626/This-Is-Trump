2666c597bef3ecb94f7a9a66801273c8f5fbc39b
Brian Luebbert-Hill <BLuebbertHill@gmail.com>
2016-10-22 23:15:29 -0400

    A THING

---
+++ Trump - An Interactive Experience/Assets/Scripts/BGScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroll : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeX;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+		transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
